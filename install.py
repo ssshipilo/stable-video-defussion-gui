a36c3ee7bcd4b86542a41d4006344cb7e32bb0b5
ssshipilo <ssshipilo@gmail.com>
2023-12-28 19:53:37 +0100

    comit

--- install.py
+++ install.py
@@ -23,6 +23,9 @@ subprocess.run(['pip', 'install', 'invisible-watermark'])
 subprocess.run(['pip', 'install', 'SciPy'])
 subprocess.run(['pip', 'install', 'torch==2.1.0+cu118', 'torchvision==2.1.0+cu118', 'torchaudio==2.1.0+cu118', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
 subprocess.run(['pip', 'install', 'xformers'])
+subprocess.run(['pip', 'install', '--ignore-installed', 'Flask'])
+subprocess.run(['pip', 'install', 'Flask-SocketIO'])
+
 # CUDA ограничитель 128, потому что ему хоть дай 80 гб, он всё сожрёт
 subprocess.run(['PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128'])
 
@@ -31,10 +34,18 @@ os.makedirs('checkpoints', exist_ok=True)
 
 # Скачивание модели обученной на 25 кадрах
 print("\033[92m[AUTO INSTALL]\033[0m Download an enlarged model trained on video at 25 fps")
-subprocess.run(['wget', '-O', './checkpoints/svd_xt.safetensors', 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid-xt/resolve/main/svd_xt.safetensors?download=true'])
+try:
+    result = subprocess.run(['wget', '-O', './checkpoints/svd_xt.safetensors', 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid-xt/resolve/main/svd_xt.safetensors?download=true'], check=True)
+    print(result.stdout)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
 print("\033[92m[AUTO INSTALL]\033[0m Successful!!!")
 
 # Скачивание более легкой модели обученной на 14 кадрах
 print("\033[92m[AUTO INSTALL]\033[0m Download an enlarged model trained on video at 14 fps")
-subprocess.run(['wget', '-O', './checkpoints/svd.safetensors', 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors?download=true'])
+try:
+    result = subprocess.run(['wget', '-O', './checkpoints/svd.safetensors', 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors?download=true'], check=True)
+    print(result.stdout)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
 print("\033[92m[AUTO INSTALL]\033[0m Successful!!!")
--- server.py
+++ server.py
@@ -1,28 +1,43 @@
 from flask import Flask, render_template, send_from_directory, request, jsonify
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 import uuid
 import psutil
 import datetime
-import cv2 
+import time
+from threading import Thread
 from model import sample
 from fire import Fire
-import os
+from multiprocessing import Manager
+from multiprocessing import Process, Value
 import shutil
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 socketio = SocketIO(app)
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
-    
-#TODO: DONE!
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-#TODO: DONE!
+if not os.path.exists('outputs'):
+    os.makedirs("outputs")
+
+p = None
+active_tasks = []
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    process_id = db.Column(db.Integer)
+    status = db.Column(db.Boolean)
+    message = db.Column(db.String(255))
+    time_start = db.Column(db.Float)
+    time_end = db.Column(db.Float)
+
 def send_update():
     """
     Отправка события update_files с текущими данными по сокету.
@@ -43,15 +58,24 @@ def send_update():
     used_memory = round(memory_info.used / (1024 ** 2), 2)
     free_memory = round(memory_info.available / (1024 ** 2), 2)
 
-    socketio.emit('update_files', {'files': files, 'total_memory': total_memory, 'used_memory': used_memory, 'free_memory': free_memory})
+    with app.app_context():
+        tasks = Task.query.all()
+        task_data = [{'id': task.id, 'process_id': task.process_id, 'status': task.status,
+                    'message': task.message, 'time_start': task.time_start, 'time_end': task.time_end}
+                    for task in tasks]
 
-#TODO: DONE!
+    socketio.emit('update_files', {'task': task_data, 'files': files, 'total_memory': total_memory,
+                                   'used_memory': used_memory, 'free_memory': free_memory})
+
 def background_task():
     while True:
         send_update()
         socketio.sleep(2)
 
-#TODO: DONE!
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/get_files', methods=['GET'])
 def get_files_route():
     """
@@ -60,7 +84,6 @@ def get_files_route():
     send_update()
     return jsonify({'message': 'Data sent successfully'})
 
-#TODO: DONE!
 @app.route('/delete_file/<token>', methods=['DELETE'])
 def delete_file(token):
     """
@@ -82,8 +105,7 @@ def delete_file(token):
                 return jsonify({'error': 'File not found'})
         
     return jsonify({'error': 'File not found'})
-    
-#TODO: DONE!
+
 def get_filename_by_token(token):
     """
     Получаем путь к файлу, по токену (названию файла)
@@ -95,48 +117,74 @@ def get_filename_by_token(token):
             return file
             break
 
-#TODO: DONE!
-@app.route('/download/<filename>')
-def download_file(filename):
-    """
-    Получение файла по ссылке, для скачивания
-    """
+@app.route('/delete_task/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if task:
+            db.session.delete(task)
+            db.session.commit()
+            return jsonify({'message': 'Task deleted successfully'}), 200
+        else:
+            return jsonify({'error': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-    folder_path = os.path.join(os.getcwd(), 'outputs')
-    return send_from_directory(folder_path, filename, as_attachment=True)
+@app.route('/clear_tasks', methods=['DELETE'])
+def clear_tasks():
+    try:
+        Task.query.delete()
+        db.session.commit()
+        return jsonify({'message': 'All database deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
 
-# GENERATION SVD
-def allowed_file(filename):
-    """
-    Проверка разрешенных расширений файлов
-    """
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def generate_video_process(input_path, num_frames, num_steps, version, fps_id, motion_bucket_id, cond_aug, seed,
+                            decoding_t, device, output_folder, task_result, process_pid):
+    try:
+        Fire(sample(
+            input_path,
+            num_frames,
+            num_steps,
+            version,
+            fps_id,
+            motion_bucket_id,
+            cond_aug,
+            seed,
+            decoding_t,
+            device,
+            output_folder
+        ))
 
-#TODO: DONE!
+        with app.app_context():
+            update_task(process_pid.value, True, "The video has been successfully created", time.time())
+            active_tasks.append(task_result)
+            print('The video has been successfully created')
+    except Exception as e:
+        with app.app_context():
+            update_task(process_pid.value, False, str(e), time.time())
+            print(e)
+            task_result.value = False
+
+def update_task(process_id, status, message, time_end):
+    task = Task.query.filter_by(process_id=process_id).first()
+
+    if task:
+        task.status = status
+        task.message = message
+        task.time_end = time_end
+        db.session.commit()
+
 @app.route('/generatevideo', methods=['POST'])
 def generate_video():
-    """
-    ### Генерация видео, запрос принимает запросы
-
-    #### Путь к картинке - Это путь к изображению, которое будет использоваться для создания видео.\n
-    `input_path`          = os.path.join(os.getcwd(), "assets", "example.jpg")\n
-    `num_frames`          = 25    # Количесвто фреймов\n
-    `num_steps`           = 15    # Количество шагов - Это параметр, указывающий, сколько шагов выполнить при создании видео.\n
-    `version`             = "svd_xt" # or 'svd_xt'\n
-    `fps_id`              = 6     # Число кадров в секунду - Это количество кадров в секунду для создаваемого видео.\n
-    `motion_bucket_id`    = 127   # Идентификатор "motion bucket" - Это идентификатор, связанный с движением, который влияет на результат видео.\n
-    `cond_aug`            = 0.02  # Условное увеличение - Это параметр, связанный с аугментацией данных.\n
-    `seed`                = 23    # Зерно для генерации случайных чисел - Это начальное значение для генератора случайных чисел.\n
-    `decoding_t`          = 5    # Количество кадров, декодируемых за один раз! Это съедает больше всего VRAM. При необходимости уменьшите.
-    `device`              = "cuda" # or 'cpu'
-    `output_folder`       =  os.path.join(os.getcwd(), "outputs")
-    """
-
     try:
         uploaded_files = []
         if 'file' not in request.files:
             return jsonify({"message": "No file part", "status": False}), 400
+        files = request.files.getlist('file')
+        if not files:
+            return jsonify({"message": "No files uploaded", "status": False}), 400
 
         uploads_folder = os.path.join(os.getcwd(), "uploads")
         for file_name in os.listdir(uploads_folder):
@@ -168,48 +216,58 @@ def generate_video():
         seed = request.form.get('seed')
         decoding_t = request.form.get('decoding_t')
 
-        # GENERATE
-        print(select_version)
-        input_path          = os.path.join(os.getcwd(), "uploads", uploaded_files[0])
-        num_frames          = int(fps_version)          # Количесвто фреймов
-        num_steps           = int(num_steps)            # Количество шагов - Это параметр, указывающий, сколько шагов выполнить при создании видео.
-        version             = str(select_version)       # or 'svd_xt'
-        fps_id              = int(fps_id)               # Число кадров в секунду - Это количество кадров в секунду для создаваемого видео.
-        motion_bucket_id    = int(motion_bucket)        # Идентификатор "motion bucket" - Это идентификатор, связанный с движением, который влияет на результат видео.
-        cond_aug            = float(cond_aug)           # Условное увеличение - Это параметр, связанный с аугментацией данных.
-        seed                = int(seed)                 # Зерно для генерации случайных чисел - Это начальное значение для генератора случайных чисел.
-        decoding_t          = int(decoding_t)           # Количество кадров, декодируемых за один раз! Это съедает больше всего VRAM. При необходимости уменьшите.
-        device              = "cuda"                    # or 'cpu'
-        output_folder       =  os.path.join(os.getcwd(), "outputs")
+        input_path = os.path.join(os.getcwd(), "uploads", uploaded_files[0])
+        num_frames = int(fps_version)
+        num_steps = int(num_steps)
+        version = str(select_version)
+        fps_id = int(fps_id)
+        motion_bucket_id = int(motion_bucket)
+        cond_aug = float(cond_aug)
+        seed = int(seed)
+        decoding_t = int(decoding_t)
+        device = "cuda"
+        output_folder = os.path.join(os.getcwd(), "outputs")
+        
+        process_pid = Value('i', 0)
+        p = Process(target=generate_video_process, args=(input_path, num_frames, num_steps, version, fps_id, motion_bucket_id,
+                                                         cond_aug, seed, decoding_t, device, output_folder, task_result, process_pid))
+        p.start()
+        if p is not None:
+            process_pid.value = p.pid
 
-        Fire(sample(
-            input_path,
-            num_frames,
-            num_steps,
-            version,
-            fps_id,
-            motion_bucket_id,
-            cond_aug,
-            seed,
-            decoding_t,
-            device,
-            output_folder
-        ))
+            task = Task(process_id=process_pid.value, status=None, message="Processing...", time_start=time.time(), time_end=None)
+            db.session.add(task)
+            db.session.commit()
 
-        return jsonify({"message": "Success!", "status": True})
+        return jsonify({"message": "Success!", "status": True, "task_id": process_pid.value})
     except Exception as e:
         return jsonify({"message": str(e), "status": False}), 400
 
-def generate_video(frames, output_path):
-    height, width, _ = frames[0].shape
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter(output_path, fourcc, 30.0, (width, height))
+@app.route('/download/<filename>')
+def download_file(filename):
+    """
+    Получение файла по ссылке, для скачивания
+    """
 
-    for frame in frames:
-        video_writer.write(frame)
+    folder_path = os.path.join(os.getcwd(), 'outputs')
+    return send_from_directory(folder_path, filename, as_attachment=True)
 
-    video_writer.release()
+def allowed_file(filename):
+    """
+    Проверка разрешенных расширений файлов
+    """
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
-    socketio.start_background_task(target=background_task)
-    socketio.run(app, debug=True, port=5434)
+    # pip install python-dotenv
+    # flask db init
+    # flask db migrate -m "Initial migration"
+    # flask db upgrade
+
+    with app.app_context():
+        manager = Manager()
+        task_result = manager.Event()
+
+        socketio.start_background_task(target=background_task)
+        socketio.run(app, debug=True, host="0.0.0.0", port=5434)
